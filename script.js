acaa18084a6b00f8da692cca79317e74ed3777fa
Mariusz Bigos <mbrolety@gmail.com>
2023-10-16 17:36:02 +0200

    refactoring js script

--- script.js
+++ script.js
@@ -5,38 +5,38 @@ const welcome = () => {
 welcome();
 
 const rollerPrize = () => {
-    
-    const high = document.querySelector(".high").innerText;
-const howMuch = document.querySelector(".howMuch");
 
-switch (true) {
-    case true === high < 1500:
-        howMuch.innerText = "200";
-        break;
-    case true === high < 2000:
-        howMuch.innerText = "300";
-        break;
-    case true === high > 2000:
-        howMuch.innerText = "500";
-        break;
-}
-}
+    const high = document.querySelector(".high").innerText;
+    const howMuch = document.querySelector(".howMuch");
+
+    switch (true) {
+        case true === high < 1500:
+            howMuch.innerText = "200";
+            break;
+        case true === high < 2000:
+            howMuch.innerText = "300";
+            break;
+        case true === high > 2000:
+            howMuch.innerText = "500";
+            break;
+    }
+};
 
 rollerPrize();
 
-    const onChangeRollerClick = () => {
+const onChangeRollerClick = () => {
 
-        const picture = document.querySelector(".js-picture");
-        const themeName = document.querySelector(".themeName")
+    const picture = document.querySelector(".js-picture");
+    const themeName = document.querySelector(".themeName")
 
-        if (picture.src.endsWith("images/flat.jpg")) {
-            picture.src = "images/flat_p.jpg";
-            themeName.innerText = "zaciemniająca";
-        } else if (picture.src.endsWith("images/flat_p.jpg")) {
-            picture.src = "images/flat.jpg";
-            themeName.innerText = "dzień / noc";
-        }
-    };
+    if (picture.src.endsWith("images/flat.jpg")) {
+        picture.src = "images/flat_p.jpg";
+        themeName.innerText = "zaciemniająca";
+    } else if (picture.src.endsWith("images/flat_p.jpg")) {
+        picture.src = "images/flat.jpg";
+        themeName.innerText = "dzień / noc";
+    }
+};
 
 const init = () => {
 
